44d6ff7fc4dd393e87b67f7c6e59ebecfe52fef0
0xb10c <b10c@b10c.me>
2026-03-28 13:34:32 +0100

    add: flake.nix and .gitignore

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "p2p-observer dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.rustc
+              pkgs.cargo
+              pkgs.clippy
+              pkgs.rustfmt
+              pkgs.rust-analyzer
+
+              pkgs.nats-server
+            ];
+          };
+        });
+    };
+}
